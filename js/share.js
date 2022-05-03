36781b9fb660554d897e181b07aa35f730096e47
qorbaxk <bekgyber3@hanmail.net>
2022-05-03 21:16:39 +0900

    finish

--- js/share.js
+++ js/share.js
@@ -3,7 +3,7 @@ const url = "https://avengers-heroes-test.netlify.app/";
 function setShare() {
   let resultImg = document.querySelector("#resultImg");
   let resultAlt = resultImg.firstElementChild.alt;
-  const shareTitle = "내가 어벤져스라면? 결과";
+  const shareTitle = "어벤져스 입단테스트 결과";
   const shareDesc = `${infoList[resultAlt].ex}, 나는 ${infoList[resultAlt].name}!`;
   const shareImage = `${url}img/image-${resultAlt}.png`;
   const shareURL = `${url}page/result-${resultAlt}.html`;
